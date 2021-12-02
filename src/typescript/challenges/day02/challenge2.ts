2e50ba915d499bb1ef6e2025e21b9bcca5ffa047
Paul Richard <paulrichard@Pauls-MacBook-Pro.local>
2021-12-02 13:28:44 -0600

    reformat with spaces day 2 challenge 2 TS

--- src/typescript/challenges/day02/challenge2.ts
+++ src/typescript/challenges/day02/challenge2.ts
@@ -1,15 +1,15 @@
 export default (input: string) => {
-    const [finalVert, finalHorz] = input.split('\n').reduce(([vert, horz, aim], command) => {
-      const [direction, unitString] = command.split(' ');
-      const units = parseInt(unitString);
-      switch (direction) {
-        case 'up':
-          return [vert, horz, aim - units];
-        case 'down':
-          return [vert, horz, aim + units];
-        default:
-          return [vert + (aim * units), horz + units, aim];
-      }
-    }, [0, 0, 0]);
-    return Math.abs(finalVert * finalHorz);
-  };
+  const [finalVert, finalHorz] = input.split('\n').reduce(([vert, horz, aim], command) => {
+    const [direction, unitString] = command.split(' ');
+    const units = parseInt(unitString);
+    switch (direction) {
+      case 'up':
+        return [vert, horz, aim - units];
+      case 'down':
+        return [vert, horz, aim + units];
+      default:
+        return [vert + (aim * units), horz + units, aim];
+    }
+  }, [0, 0, 0]);
+  return Math.abs(finalVert * finalHorz);
+};
