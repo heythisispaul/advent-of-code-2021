ee26d4c609839c660c15dda3dc7f11c63d5d4ca6
Paul Richard <paulrichard@Pauls-MacBook-Pro.local>
2021-12-03 13:10:19 -0600

    day 3 challenge 1 TS

---
+++ src/typescript/challenges/day03/challenge1.ts
@@ -0,0 +1,19 @@
+export default (input: string) => {
+  const binaryValues = input.split('\n');
+  const toDecimal = (binaryArr: number[]) => parseInt(binaryArr
+    .map((value) => value.toString()).join(''), 2);
+
+  const summedBits = binaryValues.reduce((accrued: number[], current) => {
+    current.trim().split('').forEach((value, index) => {
+      accrued[index] += parseInt(value);
+    });
+    return accrued;
+  }, new Array(binaryValues[0].length).fill(0, 0));
+
+  const gamma = summedBits.map((value) => (
+    value > (binaryValues.length / 2) ? 1 : 0
+  ));
+  const epsilon = gamma.map((value) => value > 0 ? 0 : 1);
+
+  return (toDecimal(gamma) * toDecimal(epsilon));
+};
