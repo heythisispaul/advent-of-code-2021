35b17af887b0cc5ecdf11caf296ef410e2b09acc
Paul Richard <paulrichard@Pauls-MacBook-Pro.local>
2021-12-07 12:31:02 -0600

    d7c1 TS solution

---
+++ src/typescript/challenges/day07/challenge1.ts
@@ -0,0 +1,15 @@
+export default (input: string) => {
+  const numberArray = input.split(',').map(parseFloat);
+  const occurrences = numberArray.reduce((counter, number) => {
+    counter[number] = counter[number] + 1;
+    return counter;
+  }, new Array<number>(Math.max(...numberArray) + 1).fill(0, 0));
+
+  const fuelTotals = occurrences
+    .map((_, number) => occurrences
+      .reduce((fuelTotal, occurs, currentNumber) => (
+        fuelTotal + (Math.abs(number - currentNumber) * occurs)
+    ), 0));
+
+  return Math.min(...fuelTotals);
+};
